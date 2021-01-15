db3d35304c803aea65f0eae4ca0a47a97eb3bb59
Agentzhao <tan.hongzhao@dhs.sg>
2021-01-15 20:54:16 +0800

    Create val err 43.bat

---
+++ val err 43.bat
@@ -0,0 +1,7 @@
+rem edit file path for RiotClientSettings.yaml
+cd C:\Users\User\AppData\Local\Riot Games\Riot Client\Config
+del /f RiotClientSettings.yaml
+
+rem edit file path for VALORANT to auto start
+cd C:\Users\User\Desktop\games
+start VALORANT
